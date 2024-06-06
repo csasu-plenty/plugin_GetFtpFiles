30bd1cf976991af3699b006d74e6b54c072248f0
Ciprian Sasu <>
2024-06-06 14:53:37 +0300

    upload code

---
+++ src/Providers/GetFtpFilesRouteServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace GetFtpFiles\Providers;
+
+use Plenty\Plugin\Routing\ApiRouter;
+use Plenty\Plugin\RouteServiceProvider;
+
+/**
+ * Class GetFtpFilesRouteServiceProvider
+ * @package GetFtpFiles\Providers
+ */
+class GetFtpFilesRouteServiceProvider extends RouteServiceProvider
+{
+    /**
+     * @param  ApiRouter  $apiRouter
+     */
+    public function map(ApiRouter $apiRouter)
+    {
+        $apiRouter->version(['v1'], ['namespace' => 'GetFtpFiles\Controllers', 'middleware' => 'oauth'],
+            function ($apiRouter) {
+                $apiRouter->get('GetFtpFiles/import_images/', 'TestController@importImages');
+            }
+        );
+    }
+}
